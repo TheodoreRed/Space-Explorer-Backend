50dfc79b2f640fa3ca439b0c21c2fe66b0470309
Theodore Redlon <redlontheo@gmail.com>
2023-12-09 13:56:43 -0500

    initial

---
+++ functions/src/index.ts
@@ -0,0 +1,12 @@
+import * as functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import accountRouter from "./routes/accountsRouter";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/", accountRouter);
+
+export const api = functions.https.onRequest(app);
